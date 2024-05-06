8b10a8e6c5dab388ea4bb138d38f35b04d2d2e0f
cormochamelion <cormocodran1.5@gmail.com>
2024-05-06 22:59:31 +0200

    main: Format file
      Praise the `rustfmt`.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod structs;
 use std::env::var;
 use std::fs;
 
+use crate::structs::Punlines;
 use chrono::{Datelike, Weekday};
-use frankenstein::{AsyncApi, BotCommand, GetUpdatesParams, Message, ReplyParameters, SendAnimationParams, SendPollParams, SetMyCommandsParams, UpdateContent};
 use frankenstein::AsyncTelegramApi;
+use frankenstein::{
+    AsyncApi, BotCommand, GetUpdatesParams, Message, ReplyParameters, SendAnimationParams,
+    SendPollParams, SetMyCommandsParams, UpdateContent,
+};
 use rand::prelude::IteratorRandom;
 use serde::{Deserialize, Serialize};
-use crate::structs::Punlines;
 
 static CHAT_ID: i64 = 231642019;
 
@@ -16,7 +19,7 @@ static CHAT_ID: i64 = 231642019;
 async fn main() {
     match var("BOT_TOKEN") {
         Ok(val) => connect_to_api(val).await,
-        Err(..) => println!("Needs token to run!")
+        Err(..) => println!("Needs token to run!"),
     }
 }
 
@@ -26,8 +29,13 @@ async fn connect_to_api(token: String) {
     let update_params_builder = GetUpdatesParams::builder();
     let mut update_params = update_params_builder.clone().build();
 
-    let dodo_command = BotCommand::builder().command("dodo").description("Polls for dota availability").build();
-    let set_my_commands_params = SetMyCommandsParams::builder().commands(vec![dodo_command]).build();
+    let dodo_command = BotCommand::builder()
+        .command("dodo")
+        .description("Polls for dota availability")
+        .build();
+    let set_my_commands_params = SetMyCommandsParams::builder()
+        .commands(vec![dodo_command])
+        .build();
 
     if let Err(..) = api.set_my_commands(&set_my_commands_params).await {
         println!("Failed to set commands");
@@ -58,7 +66,10 @@ async fn connect_to_api(token: String) {
                         });
                     }
 
-                    update_params = update_params_builder.clone().offset(update.update_id + 1).build();
+                    update_params = update_params_builder
+                        .clone()
+                        .offset(update.update_id + 1)
+                        .build();
                 }
             }
             Err(_error) => {
@@ -72,9 +83,15 @@ async fn process_message(message: Message, api: AsyncApi) {
     // Check for doubt and its other text forms
     if let Some(ref message_content) = message.text {
         if message_content.contains("doubt") || message_content.contains("daut") {
-            let reply_parameters = ReplyParameters::builder().message_id(message.message_id).build();
+            let reply_parameters = ReplyParameters::builder()
+                .message_id(message.message_id)
+                .build();
             let file_path = std::path::PathBuf::from("resources/i_daut_it.gif");
-            let send_animation_params = SendAnimationParams::builder().chat_id(CHAT_ID).animation(file_path).reply_parameters(reply_parameters).build();
+            let send_animation_params = SendAnimationParams::builder()
+                .chat_id(CHAT_ID)
+                .animation(file_path)
+                .reply_parameters(reply_parameters)
+                .build();
 
             if let Err(err) = api.send_animation(&send_animation_params).await {
                 println!("Failed to send message: {err:?}");
@@ -85,7 +102,6 @@ async fn process_message(message: Message, api: AsyncApi) {
         }
     }
 }
-
 
 fn get_day_prefix() -> &'static str {
     let current_time = chrono::offset::Local::now();
@@ -102,7 +118,8 @@ fn get_day_prefix() -> &'static str {
 
 async fn send_dodo_poll(api: AsyncApi, message: Option<Message>) {
     let result: Punlines = {
-        let file_content = fs::read_to_string("resources/punlines.json").expect("Error reading punlines json");
+        let file_content =
+            fs::read_to_string("resources/punlines.json").expect("Error reading punlines json");
         serde_json::from_str(&file_content).unwrap()
     };
 
@@ -113,13 +130,24 @@ async fn send_dodo_poll(api: AsyncApi, message: Option<Message>) {
     let no_answer = no_answers.iter().choose(&mut rand::thread_rng()).unwrap();
 
     if let Some(message) = message {
-        let reply_parameters = ReplyParameters::builder().message_id(message.message_id).build();
-        let send_poll_params = SendPollParams::builder().reply_parameters(reply_parameters).chat_id(CHAT_ID).question("Dodo?").options(vec![yes_answer.clone(), no_answer.clone()]).build();
+        let reply_parameters = ReplyParameters::builder()
+            .message_id(message.message_id)
+            .build();
+        let send_poll_params = SendPollParams::builder()
+            .reply_parameters(reply_parameters)
+            .chat_id(CHAT_ID)
+            .question("Dodo?")
+            .options(vec![yes_answer.clone(), no_answer.clone()])
+            .build();
         if let Err(err) = api.send_poll(&send_poll_params).await {
             println!("Failed to send message: {err:?}");
         }
     } else {
-        let send_poll_params = SendPollParams::builder().chat_id(CHAT_ID).question("Do".to_owned() + get_day_prefix()).options(vec![yes_answer.clone(), no_answer.clone()]).build();
+        let send_poll_params = SendPollParams::builder()
+            .chat_id(CHAT_ID)
+            .question("Do".to_owned() + get_day_prefix())
+            .options(vec![yes_answer.clone(), no_answer.clone()])
+            .build();
         if let Err(err) = api.send_poll(&send_poll_params).await {
             println!("Failed to send message: {err:?}");
         }
